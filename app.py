071c578681c5ab3d9da353b1b720aed0170a4078
Voice-bot-user <solimax.de@gmail.com>
2025-05-04 10:00:47 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, Response
+from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
 
-@app.route("/voice", methods=['POST'])
+@app.route("/voice", methods=["POST"])
 def voice():
     resp = VoiceResponse()
     resp.say("Привет! Это голосовой бот на Render и Twilio.", language="ru-RU")
-    return Response(str(resp), mimetype='application/xml')
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=10000)
